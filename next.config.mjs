724ce2c7282dea8911312b4bdccd61673ba97de2
Nikhil Kushwaha <osndl.nikhilk@aicofindia.com>
2025-02-24 00:21:31 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    appDir: true, // If using Next.js App Router
+  },
+};
 
 export default nextConfig;
